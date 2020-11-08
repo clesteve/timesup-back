773990d14aeb96892b5e2deb8930460f9ec0990b
clement.esteve <clement.esteve@thalesdigital.io>
2020-11-08 02:36:17 +0100

    functional

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 import os
 import redis
 import json
-from flask import Flask, session, request, jsonify, make_response
+from flask import Flask, session, request, jsonify, make_response, Response
+from flask_cors import CORS  # type: ignore
 from dotenv import load_dotenv  # type: ignore
+from datetime import datetime
+from random import shuffle
+from uuid import uuid4
+
 
 from game import Game
 
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = os.getenv('FLASK_SECRET')
 red = redis.StrictRedis()
+
+
+def event_stream(game_id):
+    pubsub = red.pubsub()
+    pubsub.subscribe(game_id.encode('utf-8'))
+    event = pubsub.get_message()
+    for message in pubsub.listen():
+        if message['type'] == 'message':
+            yield 'data: %s\n\n' % message['data'].decode('utf-8')
 
 
 def get_game(game_id: str) -> Game:
@@ -27,38 +42,163 @@ def get_game(game_id: str) -> Game:
 
 @app.route('/games')
 def index():
+    # type: ignore
     return jsonify({'games': [i.decode('utf-8') for i in red.scan_iter()]})
 
 
 @app.route('/games', methods=['POST'])
 def create_game_route():
-    user = request.args['user']
-    game = Game(users=[user])
-    red.set(game.id, json.dumps(game.__dict__))
-    return jsonify({'game': game.__dict__})
+    user = request.args['username']
+    game = Game(users=[user], admin=request.args['username'])
+    red.set(game.id, game.toJSON())
+    return jsonify({'gameId': game.id})
 
 
 @app.route('/games/<id>', methods=['GET'])
 def get_game_route(id):
     game = get_game(id)
-    print(game)
-    username = request.args['user']
+    username = request.args['username']
     if username not in game.users:
         game.add_user(username)
-    return jsonify({'game': game.__dict__})
+        red.set(id, game.toJSON())
+        red.publish(id, game.visibleToJSON())
+    return jsonify({'game': game.visibleToJSON()})
 
 
-@app.route('/games/<id>', methods=['POST'])
+@app.route('/quit/<id>', methods=['GET'])
+def quit_game(id):
+    game = get_game(id)
+    username = request.args['username']
+    if username in game.users:
+        game.users.remove(username)
+        red.set(id, game.toJSON())
+        red.publish(id, game.visibleToJSON())
+    if len(game.users) == 0:
+        red.delete(id)
+    return jsonify({'deconnected': True})
+
+
+@app.route('/characters/<id>', methods=['POST'])
 def update_game_route(id):
-    """
-    TODO
-    """
-    pass
+    game = get_game(id)
+    username = request.json['username']
+
+    if 'characters' in request.json:
+        for c in request.json['characters']:
+            cid = str(uuid4())
+            game.characters[cid] = {
+                'id': cid, 'name': c, 'submitted': username, 'discovered': ['', '', '']}
+
+    if username not in game.submitted:
+        game.submitted.append(username)
+
+    game.make_discover_list()
+
+    red.set(game.id, game.toJSON())
+    red.publish(id, game.visibleToJSON())
+
+    return jsonify({'acknowledged': True})
+
+
+@app.route('/round/<id>')
+def next_round(id):
+    game = get_game(id)
+    game.round += 1
+    red.set(game.id, game.toJSON())
+    red.publish(id, game.visibleToJSON())
+    return jsonify({'success': True})
+
+
+@app.route('/stream/<id>', methods=['GET'])
+def get_stream_route(id):
+    return Response(event_stream(id),
+                    mimetype="text/event-stream")
+
+
+@app.route('/teams/<id>', methods=['GET'])
+def generateTeams(id):
+    game = get_game(id)
+    if len(game.users) < 4:
+        raise Exception("Impossible to play with less than 4 players")
+    if len(game.users) % 2:
+        raise Exception("Impossible to play with an odd number of players")
+
+    shuffle(game.users)
+    game.teams = [[game.users[i], game.users[i +
+                                             int(len(game.users)/2)]] for i in range(int(len(game.users)/2))]
+
+    red.set(game.id, game.toJSON())
+    red.publish(id, game.visibleToJSON())
+    return jsonify({'success': True})
+
+
+@app.route('/chrono/<id>')
+def start_chrono(id):
+    game = get_game(id)
+    game.start_chrono()
+    red.set(game.id, game.toJSON())
+    red.publish(id, game.visibleToJSON())
+    return jsonify({'success': True, 'character': game.characters[game.to_discover[0]]})
+
+
+@app.route('/player/<id>')
+def next_player(id):
+    game = get_game(id)
+    game.next_user()
+    red.set(game.id, game.toJSON())
+    red.publish(id, game.visibleToJSON())
+    return jsonify({'success': True})
+
+
+@app.route('/discovered/<id>')
+def character_discovered(id):
+    game = get_game(id)
+    game.discover(request.args['character'], request.args['username'])
+    if len(game.to_discover):
+        next_character = game.characters[game.to_discover[0]]
+        red.set(game.id, game.toJSON())
+        red.publish(id, game.visibleToJSON())
+        return jsonify({'succes': True, 'character': next_character, 'end': False})
+    elif game.round < 2:
+        game.round += 1
+        game.make_discover_list()
+        next_character = game.characters[game.to_discover[0]]
+        game.next_user()
+        red.set(game.id, game.toJSON())
+        red.publish(id, game.visibleToJSON())
+        return jsonify({'succes': True, 'character': next_character, 'end': False})
+    else:
+        game.end = True
+        red.set(game.id, game.toJSON())
+        red.publish(id, game.visibleToJSON())
+        return jsonify({'succes': True, 'character': {}, 'end': True})
+
+
+@app.route('/pass/<id>')
+def pass_card(id):
+    game = get_game(id)
+    to_pass = game.to_discover[0]
+    game.to_discover.pop(0)
+    game.to_discover.append(to_pass)
+    red.set(game.id, game.toJSON())
+    return jsonify({'character': game.characters[game.to_discover[0]]})
+
+
+@app.route('/results/<id>')
+def getResults(id):
+    game = get_game(id)
+    return jsonify({'game': game.toJSON()})
+
+
+@app.route('/character/<id>')
+def getCharacter(id):
+    game = get_game(id)
+    return jsonify({'character': game.characters[game.to_discover[0]]})
 
 
 @app.errorhandler(Exception)
 def handleException(e):
-    return make_response(jsonify({'error': str(e)}), 500)
+    return make_response(jsonify({'message': str(e)}), 500)
 
 
 if __name__ == '__main__':
--- game.py
+++ game.py
@@ -1,37 +1,18 @@
 from uuid import uuid4
 from typing import List
+from datetime import datetime
 import json
+from random import shuffle
 
 
 class Character():
     """
     """
 
-    def __init__(self, name: str, discovered=""):
+    def __init__(self, name: str, submitted: str, discovered=""):
         self.name = name
         self.discovered = discovered
-
-    def is_discovered(self, by: str):
-        self.discovered = by
-
-    def __repr__(self):
-        return self.__dict__
-
-    def _str___(self):
-        return self.__dict__
-
-
-class Team():
-    def __init__(self, users=[], id=str(uuid4())):
-        self.users = users
-        self.id = id
-
-    def add_user(self, username):
-        if username not in self.users:
-            self.users.append(username)
-        else:
-            raise Exception(
-                "Already a user with name {} in this team".format(username))
+        self.submitted = submitted
 
 
 class Game():
@@ -46,8 +27,7 @@ class Game():
         The players in this game
     teams : List[Teams]
         List of teams in the game
-    characters : List[Character]
-        A list of characters (cf class Character)
+    characters : List[dict]
     round : Current game round 
 
     Methods
@@ -56,21 +36,35 @@ class Game():
         Add a player to the player list
     """
 
-    def __init__(self, id=str(uuid4()), users=[], characters: List[Character] = [], teams: List[Team] = [], round=1):
+    def __init__(self, id=str(uuid4()), users=[], characters: dict = {}, teams: List[List[str]] = [], round=-1, admin="", submitted=[]):
         self.id = id
         self.users = users
         self.characters = characters
+        self.submitted = submitted
         self.teams = teams
         self.round = round
+        self.current_user = 0
+        self.chrono = {'started': False, 'begin': datetime.now().isoformat()}
+        self.admin = admin
+        self.to_discover: list = []
+        self.end = False
+
+    def make_discover_list(self):
+        self.to_discover = list(self.characters.keys())
+        shuffle(self.to_discover)
 
     def from_dict(self, game_dict: dict):
         self.id = game_dict['id']
         self.users = game_dict['users']
-        self.characters = [Character(c['name'], discovered=c['discovered'])
-                           for c in game_dict['characters']]
-        self.teams = [Team(users=t['users'], id=t['id'])
-                      for t in game_dict['teams']]
+        self.characters = game_dict['characters']
+        self.submitted = game_dict['submitted']
+        self.teams = game_dict['teams']
+        self.current_user = game_dict['current_user']
+        self.chrono = game_dict['chrono']
         self.round = game_dict['round']
+        self.admin = game_dict['admin']
+        self.to_discover = game_dict['to_discover']
+        self.end = game_dict['end']
 
     def add_user(self, username: str):
         if username not in self.users:
@@ -79,15 +73,37 @@ class Game():
             raise Exception(
                 "Already a user with name {} in this game".format(username))
 
-    def add_team(self, team: Team):
-        if team not in self.teams:
-            self.teams.append(team)
-        else:
-            raise Exception(
-                "This team is already in the game")
+    def next_user(self):
+        self.current_user = (self.current_user + 1) % len(self.users)
+        self.chrono = {'started': False, 'begin': datetime.now().isoformat()}
+
+    def start_chrono(self):
+        self.chrono = {'started': True,
+                       'begin': datetime.now().isoformat()}  # type: ignore
+
+    def discover(self, character, username):
+        self.to_discover.remove(character)
+        self.characters[character]['discovered'][self.round] = username
 
     def __repr__(self):
-        return self.__dict__
+        return json.dumps(self.__dict__)
 
     def __str__(self):
         return json.dumps(self.__dict__)
+
+    def toJSON(self):
+        game_dict = self.__dict__
+        return json.dumps(game_dict)
+
+    def visibleToJSON(self):
+        to_send = {
+            'users': self.users,
+            'teams': self.teams,
+            'round': self.round,
+            'current_user': self.current_user,
+            'chrono': self.chrono,
+            'admin': self.admin,
+            'submitted': self.submitted,
+            'end': self.end
+        }
+        return json.dumps(to_send)
